0ee95015b6f3ae3ea12fcc39f8497e8be245d603
kiyoopoon <r932059@gmail.com>
2024-11-03 16:21:04 +0600

    merge .config files with existing GitHub repository

--- .launch_waybar.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-
-killall waybar
-sleep 0.5
-waybar &
--- hypr/cliboard_manager.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-selection=$(cliphist list | wofi --dmenu)
-[ -z "$selection" ] && exit
-
-content=$(cliphist decode "$selection")
-echo -n "$content" | wl-copy
-notify-send "Clipboard" "Content copied to clipboard" -t 2000
